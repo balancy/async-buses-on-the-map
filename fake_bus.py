162cbe17a3f39f64d27ede032c8564fc95418af0
balancy <balancy@gmail.com>
2022-03-08 15:16:03 +0100

    Make client launch all buses

--- fake_bus.py
+++ fake_bus.py
@@ -4,35 +4,35 @@ from sys import stderr
 import trio
 from trio_websocket import open_websocket_url
 
-ROUTES = 'routes/156.json'
+from load_routes import load_routes
+
 TIMEOUT = 1
 
 
-def extract_routes(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-async def launch_bus():
-    routes = extract_routes(ROUTES)
+async def run_bus(route, ws):
     message = {
         'msgType': 'Buses',
         'buses': [
             {
-                'busId': f'{routes["name"]}-0',
-                'route': routes['name'],
+                'busId': f'{route["name"]}-0',
+                'route': route['name'],
             },
         ],
     }
+    for coordinates in route['coordinates']:
+        message['buses'][0].update(
+            {'lat': coordinates[0], 'lng': coordinates[1]}
+        )
+        await ws.send_message(json.dumps(message, ensure_ascii=False))
+        await trio.sleep(TIMEOUT)
 
+
+async def launch_bus():
     try:
         async with open_websocket_url('ws://127.0.0.1:8080') as ws:
-            for coordinates in routes['coordinates']:
-                message['buses'][0].update(
-                    {'lat': coordinates[0], 'lng': coordinates[1]}
-                )
-                await ws.send_message(json.dumps(message))
-                await trio.sleep(TIMEOUT)
+            async with trio.open_nursery() as nursery:
+                for route in load_routes():
+                    nursery.start_soon(run_bus, route, ws)
 
     except OSError as ose:
         print('Connection attempt failed: %s' % ose, file=stderr)
